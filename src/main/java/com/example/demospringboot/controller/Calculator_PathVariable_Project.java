204621753a7dc614336734c1b1028e5e5aa22129
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-06-25 10:03:01 +0530

    Calculator Project Pathvariable

--- src/main/java/com/example/demospringboot/controller/Calculator_PathVariable_Project.java
+++ src/main/java/com/example/demospringboot/controller/Calculator_PathVariable_Project.java
@@ -1,4 +1,15 @@
 package com.example.demospringboot.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Calculator_PathVariable_Project {
+    @RequestMapping("calculator{a,b}")
+    public String calculator(@PathVariable int a,@PathVariable int b){
+        int addition;
+        addition=a+b;
+        return "Addition of given number is :- "+addition;
+    }
 }
