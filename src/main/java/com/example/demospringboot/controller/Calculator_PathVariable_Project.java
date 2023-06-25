75bd2d22c2cb4c975f7493113f54822e0b672905
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-06-25 10:45:15 +0530

    Calculator Project Pathvariable Complete- Day 1

--- src/main/java/com/example/demospringboot/controller/Calculator_PathVariable_Project.java
+++ src/main/java/com/example/demospringboot/controller/Calculator_PathVariable_Project.java
@@ -6,10 +6,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Calculator_PathVariable_Project {
-    @RequestMapping("calculator{a,b}")
-    public String calculator(@PathVariable int a,@PathVariable int b){
-        int addition;
-        addition=a+b;
-        return "Addition of given number is :- "+addition;
+    @RequestMapping("calc_addition/{a}/{b}")
+    public String calc_addition(@PathVariable int a,@PathVariable int b ){
+        int sum=a+b;
+        return "Addition of given number is :- "+sum;
+    }
+
+    @RequestMapping("calc_subtraction/{a}/{b}")
+    public String calc_subtraction(@PathVariable int a,@PathVariable int b ){
+        int sub=a-b;
+        return "Subtraction of given number is :- "+sub;
+    }
+    @RequestMapping("calc_multiply/{a}/{b}")
+    public String calc_multiply(@PathVariable int a,@PathVariable int b ){
+        int mul=a*b;
+        return "Multiply of given number is :- "+mul;
+    }
+    @RequestMapping("calc_divide/{a}/{b}")
+    public String calc_divide(@PathVariable int a,@PathVariable int b ){
+        int div=a/b;
+        return "Divide of given number is :- "+div;
     }
 }
