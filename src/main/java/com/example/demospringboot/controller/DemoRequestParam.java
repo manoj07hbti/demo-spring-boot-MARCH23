4db1418c3d5bfe94413cb046d256234fadd75fda
Manoj Kumar <manojkumar.pcf@gmail.com>
2023-06-27 20:35:02 +0530

    RequestParam Day2

---
+++ src/main/java/com/example/demospringboot/controller/DemoRequestParam.java
@@ -0,0 +1,26 @@
+package com.example.demospringboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRequestParam {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam String name){
+
+        return "Hello "+name+ " Welcome to Path variable demo..";
+    }
+
+    @RequestMapping("/vote")
+    public String voting(@RequestParam String city,@RequestParam int age){
+
+        if(age>=18 && city.equalsIgnoreCase("Agra")){
+
+            return "Person is eligible for voting in Agra...";
+        }else {
+            return "Person NOT is eligible for voting in Agra...";
+        }
+    }
+}
