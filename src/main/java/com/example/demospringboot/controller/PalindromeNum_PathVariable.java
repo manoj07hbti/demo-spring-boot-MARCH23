1fbfebde9ca815fe43b280cd5161be11cd7aaacc
vinay <sagavinay2001.sag@gmail.com>
2023-06-28 11:14:04 +0530

    PATH VARIABLE

---
+++ src/main/java/com/example/demospringboot/controller/PalindromeNum_PathVariable.java
@@ -0,0 +1,24 @@
+package com.example.demospringboot.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class PalindromeNum_PathVariable {
+    @RequestMapping("/palindrome_num/{num}")
+    public String palindromeNum(@PathVariable int num){
+        int rev=0;
+        int a=num;
+
+        while(num>0){
+            rev=(rev*10)+num%10;
+            num=num/10;
+        }
+        if(a==rev){
+            return a+" IS THE PALINDROME NUMBER";
+        }else{
+            return a+" IS NOT THE PALINDROME NUMBER";
+        }
+    }
+}
