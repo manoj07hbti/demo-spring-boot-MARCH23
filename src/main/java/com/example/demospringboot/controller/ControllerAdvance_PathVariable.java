29d0fe77b386c87f86bd9b5dc40bc5c988bd1e21
vinay <sagavinay2001.sag@gmail.com>
2023-06-29 12:02:08 +0530

    CONTROLLER ADVANCE PATH VARIABLE

---
+++ src/main/java/com/example/demospringboot/controller/ControllerAdvance_PathVariable.java
@@ -0,0 +1,39 @@
+package com.example.demospringboot.controller;
+
+import com.example.demospringboot.model.Student;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class ControllerAdvance_PathVariable {
+    ArrayList<Student> studentArrayList=new ArrayList<>();
+    @RequestMapping("/create_student/{name}/{section}/{roll_no}")
+    public Student createstudent(@PathVariable String name,@PathVariable String section,@PathVariable int roll_no){
+        return new Student(name,section,roll_no);
+    }
+
+    @RequestMapping("/create_student_list/{name}/{section}/{roll_no}")
+    public ArrayList<Student> createStudentList(@PathVariable String name,@PathVariable String section, @PathVariable int roll_no){
+
+        Student student=new Student(name, section, roll_no);
+        studentArrayList.add(student);
+        return studentArrayList;
+    }
+
+    @RequestMapping("/find_student/{roll_no}")
+    public Student foundByRollNo( @PathVariable  int roll_no){
+        Student found_student=null;
+        for(Student student:studentArrayList){
+            if(roll_no== student.getRoll_no()){
+                found_student= student;
+            }
+        }
+        return found_student;
+    }
+}
+
+
+
---
+++ src/main/java/com/example/demospringboot/model/Student.java
@@ -0,0 +1,37 @@
+package com.example.demospringboot.model;
+
+public class Student {
+    String name;
+    String section;
+    int roll_no;
+
+    public Student(String name, String section, int roll_no) {
+        this.name = name;
+        this.section = section;
+        this.roll_no = roll_no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public int getRoll_no() {
+        return roll_no;
+    }
+
+    public void setRoll_no(int roll_no) {
+        this.roll_no = roll_no;
+    }
+}
