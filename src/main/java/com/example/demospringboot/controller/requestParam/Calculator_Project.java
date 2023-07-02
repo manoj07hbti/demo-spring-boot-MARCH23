9c92dd747dfc92e7ad35af592ca747b73533b6a7
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-07-02 11:29:29 +0530

    Calc with ReqVariable Project Complete

--- src/main/java/com/example/demospringboot/controller/requestParam/Calculator_Project.java
+++ src/main/java/com/example/demospringboot/controller/requestParam/Calculator_Project.java
@@ -7,9 +7,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Calculator_Project {
     @RequestMapping("/calAddition")
-    public String calAddition(@RequestParam int a, @RequestParam int b){
+    public String calAddition(@RequestParam int a, @RequestParam int b) {
         int sum;
-        sum=a+b;
-        return "Addition of given number is"+ sum;
+        sum = a + b;
+        return "Addition of given number is " + sum;
+    }
+
+    @RequestMapping("/calSubtraction")
+    public String calSubtraction(@RequestParam int a, @RequestParam int b) {
+        int sub;
+        sub = a - b;
+        return "Subtraction of given Number is " + sub;
+    }
+
+    @RequestMapping("/calMultiply")
+    public String calMultiply(@RequestParam int a, @RequestParam int b) {
+        int mul;
+        mul = a * b;
+        return "Multiply of given Number is " + mul;
+    }
+
+    @RequestMapping("/calDivide")
+    public String calDivide(@RequestParam int a, @RequestParam int b) {
+        int div;
+        div = a/b;
+        return "Divide of given Number is " + div;
+    }
+
+    @RequestMapping("/calPercentage")
+    public String calPercentage(@RequestParam int a, @RequestParam int b) {
+        int per;
+        per = a*b/100;
+        return "Percentage of given Number is " + per;
     }
 }
