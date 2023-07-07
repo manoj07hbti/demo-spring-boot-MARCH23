0092fca2c2dbd1e58f243887b61863848ee1c883
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-07-08 02:35:28 +0530

    CRUD Project Complete (Update not Working)

--- src/main/java/com/example/demospringboot/controller/CRUD_Project/Doctor_CRUD_PathVariable.java
+++ src/main/java/com/example/demospringboot/controller/CRUD_Project/Doctor_CRUD_PathVariable.java
@@ -1,6 +1,48 @@
 package com.example.demospringboot.controller.CRUD_Project;
 
 
+import com.example.demospringboot.model.Doctor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
 //CRUD===Meaning==(Create,Read,Update,Delete) Definition of Cred Method=======================
+
+@RestController
 public class Doctor_CRUD_PathVariable {
+    ArrayList<Doctor> doctorArrayList = new ArrayList<>();
+
+
+    @RequestMapping("/Add/{id}/{Department}/{Hospital_Name}")
+    public ArrayList<Doctor> add (@PathVariable int id, @PathVariable String Department,
+                                  @PathVariable String Hospital_Name){
+        Doctor doc=new Doctor(id,Department,Hospital_Name);
+        doctorArrayList.add(doc);
+        return doctorArrayList;
+    }
+
+    // Read Data========================================================
+    @RequestMapping("/Get_Data")
+    public ArrayList<Doctor>Read(){
+        return doctorArrayList;
+    }
+
+    // Update Data========================================================
+
+    @RequestMapping("/Update_Data/{index}/{Hospital_Name}")
+    public ArrayList<Doctor> updateData (@PathVariable int index,@PathVariable String Hospital_name){
+        Doctor updateDoc=doctorArrayList.get(index);
+        updateDoc.setHospital_Name(Hospital_name);
+        return doctorArrayList;
+    }
+
+    // Delete Data========================================================
+    @RequestMapping("/Delete_Data/{index}")
+    public ArrayList<Doctor> delete_Data(@PathVariable int index){
+        doctorArrayList.remove(index);
+        return doctorArrayList;
+     }
+
 }
--- src/main/java/com/example/demospringboot/controller/CRUD_Project/Doctor_CRUD_ReqParam.java
+++ src/main/java/com/example/demospringboot/controller/CRUD_Project/Doctor_CRUD_ReqParam.java
@@ -1,4 +1,44 @@
 package com.example.demospringboot.controller.CRUD_Project;
 
+import com.example.demospringboot.model.Doctor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
 public class Doctor_CRUD_ReqParam {
+    ArrayList<Doctor> doctorArrayList = new ArrayList<>();
+
+    //C- Create Data===================================================
+    @RequestMapping("/Add_Data")
+    public ArrayList<Doctor> addData (@RequestParam int id, @RequestParam String Department,
+                                  @RequestParam String Hospital_Name){
+        Doctor doc=new Doctor(id,Department,Hospital_Name);
+        doctorArrayList.add(doc);
+        return doctorArrayList;
+    }
+
+    // R- Read Data========================================================
+    @RequestMapping("/GetData")
+    public ArrayList<Doctor>ReadData(){
+        return doctorArrayList;
+    }
+
+    // U- Update Data========================================================
+
+    @RequestMapping("/UpdateData")
+    public ArrayList<Doctor> updateData (@RequestParam int index,@RequestParam String Hospital_name){
+        Doctor updateDoctor=doctorArrayList.get(index);
+        updateDoctor.setHospital_Name(Hospital_name);
+        return doctorArrayList;
+    }
+
+    // D- Delete Data========================================================
+    @RequestMapping("/DeleteData")
+    public ArrayList<Doctor> delete_Data(@RequestParam int index){
+        doctorArrayList.remove(index);
+        return doctorArrayList;
+    }
 }
