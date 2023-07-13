85ea73cfeec3ffd632548976ddfe31e9183c8983
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-07-13 19:56:36 +0530

    Oracle Project Testing & Assignment Complete

--- src/main/resources/SQL/Oracle.sql
+++ src/main/resources/SQL/Oracle.sql
@@ -1,3 +1,5 @@
+
+// Creating JSON=======================================
 create table Employee_Table(
    Name varchar2(40),
    Employee_ID  number,
@@ -5,12 +7,20 @@ create table Employee_Table(
 
 );
 
+//Insterting Data=========================================
+
 INSERT INTO Employee_Table (Name,Employee_ID,City) Values('Akhil',20,'Agra')/
 
+//ReadData================================================
 Select* from Employee_Table
 
+//ConditionStatement=====================================
 select* from Employee_Table where City='Agra'
 
 select Name,City from Employee_Table
 
 select* from Employee_Table where City='Delhi'
+
+//AdvanceCondStatement===================================
+
+select*from Employee_Table where rownum<6;
