ab372bee9fbf3b4e487c27f84a241dc53c11fc8a
vinay <sagarvinay2001.sag@gmail.com>
2023-07-20 11:43:11 +0530

    spring connection with database

--- src/main/java/com/example/demospringboot/model/Student.java
+++ src/main/java/com/example/demospringboot/model/Student.java
@@ -1,11 +1,23 @@
 package com.example.demospringboot.model;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name="STUDENT_MARCH")
 public class Student {
+    @Id
+    @GeneratedValue
+    long id;
     String name;
     String section;
     int roll_no;
 
-
+    //default constructor
+    public Student() {
+    }
 
     public Student(String name, String section, int roll_no) {
         this.name = name;
