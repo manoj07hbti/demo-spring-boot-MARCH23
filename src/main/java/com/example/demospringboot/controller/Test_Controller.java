1033022dde16d881f77ca150b5f879620afcbcaf
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-07-22 08:49:24 +0530

    Autowiring Test Complete

---
+++ src/main/java/com/example/demospringboot/controller/Test_Controller.java
@@ -0,0 +1,17 @@
+package com.example.demospringboot.controller;
+
+import com.example.demospringboot.service.Test_Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test_Controller {
+    @Autowired //spring Annotation to create Object====================================
+    Test_Service obj;
+    @GetMapping("/autowiring")
+    public String Demo_test(){
+
+        return obj.Business_Rule();
+    }
+}
---
+++ src/main/java/com/example/demospringboot/service/Test_Service.java
@@ -0,0 +1,12 @@
+package com.example.demospringboot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Test_Service {
+
+    public String Business_Rule(){
+
+        return "This is my Business rule.....";
+    }
+}
