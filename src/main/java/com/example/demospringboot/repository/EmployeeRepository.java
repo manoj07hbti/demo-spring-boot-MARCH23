6940e06e6998ce096d50549323db6af81551b08c
Prem Babu <premstudy123@gmail.com>
2023-07-31 19:51:15 +0530

    EmployeeRepository class or persistance layer

--- src/main/java/com/example/demospringboot/repository/EmployeeRepository.java
+++ src/main/java/com/example/demospringboot/repository/EmployeeRepository.java
@@ -2,8 +2,19 @@ package com.example.demospringboot.repository;
 
 import com.example.demospringboot.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+    public Optional<Employee> findById(Long id);
+
+    public Employee findByName(String name);
+    public Employee findByDept(String dept);
+    public Employee findBySalary(int salary); // select *from Employee where= :salary
+
+   // @Query(value = "SELECT * FROM Employee  WHERE name = :name and dept= :dept", nativeQuery = true)
+    public Employee findByNameAndId(Long id,String name);
+
+    public Employee findByNameOrSalary(String name,int salary);
 }
